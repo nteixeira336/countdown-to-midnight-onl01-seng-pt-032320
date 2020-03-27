09911354066b819e719e68ccc74eae491a6abd67
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-03-27 15:38:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+num= 12 
 
-def countdown
+def countdown (num)
+  while num > 0 
+    puts "#{num} SECOND(S)!"
+    num -= 1 
+  end 
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (num)
+  while num>0 
+    puts "#{num} SECOND(S)!"
+    num -=1 
+    sleep(1)
+  end 
+  "HAPPY NEW YEAR!"
+end 
+
